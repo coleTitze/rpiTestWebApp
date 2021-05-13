84e8347a181c7dd4316e9e6cfbbba444cd1c3ed7
coletitze <cole.titze@mines.sdsmt.edu>
2021-05-13 00:28:52 -0500

    Added directory to hold autogrower

---
+++ autoGrower/main.py
@@ -0,0 +1,8 @@
+# TODO:
+# Make a json file with a config for a automatic grower
+# Make a webpage to change the values in json
+# Have this file pull the settings and control grow equipment
+
+
+if __name__ == '__main__':
+    print("Grow things")
