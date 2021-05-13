c2ae28769643d53327895f58437678b736795ba0
coletitze <cole.titze@mines.sdsmt.edu>
2021-05-12 21:13:12 -0500

    Add test page

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'Hello world'
+
+
+@app.route('/test')
+def test():
+    return "Testing"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
