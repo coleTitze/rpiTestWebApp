4bc9d4594e764117135eaec5177b38bec7ae3407
coletitze <cole.titze@mines.sdsmt.edu>
2021-05-16 17:41:04 -0500

    Trying different use of config. Setting attribute in grab

--- static/autoGrower/plantForm.js
+++ static/autoGrower/plantForm.js
@@ -1,16 +1,3 @@
-// Get current settings from json config
-let j;
-
-$(document).ready(function(){
-    $.getJSON("static/autoGrower/config.json", function(data){
-        j = data;
-        console.log(j['waterMl'])
-    }).fail(function(){
-        console.log("An error has occurred.");
-    });
-});
-console.log(j['waterMl'])
-
 // Create a break line element
 let br = document.createElement("br");
 
@@ -26,7 +13,6 @@ let waterText = document.createTextNode("Water Per Day (ml):");
 let water = document.createElement("input");
 water.setAttribute("type", "text");
 water.setAttribute("name", "waterAmount");
-water.setAttribute("placeholder", j['waterMl']);
 
 // Create water text title
 let sunText = document.createTextNode("Light Time In Minutes Per Day:");
@@ -35,13 +21,25 @@ let sunText = document.createTextNode("Light Time In Minutes Per Day:");
 let sun = document.createElement("input");
 sun.setAttribute("type", "text");
 sun.setAttribute("name", "sunAmount");
-sun.setAttribute("placeholder", j['lightMinutes']);
 
 // create a submit button
 let submit = document.createElement("input");
 submit.setAttribute("type", "Submit");
 submit.setAttribute("name", "submitButton")
 submit.setAttribute("value", "Submit");
+
+// Get placeholders
+$(document).ready(function(){
+    $.getJSON("static/autoGrower/config.json", function(data){
+        let j = data;
+        console.log(j['waterMl'])
+        sun.setAttribute("placeholder", j['lightMinutes']);
+        water.setAttribute("placeholder", j['waterMl']);
+
+    }).fail(function(){
+        console.log("An error has occurred.");
+    });
+});
 
 // Append the water input to the form
 form.appendChild(waterText);
